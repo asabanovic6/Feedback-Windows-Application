09d535d4cc58de0eb4cb88458e3d3569f016ad6a
Amina Sabanovic <asabanovic6@etf.unsa.ba>
2021-05-18 22:16:00 +0200

    Initial commit

---
+++ Feedback-Windows-Application/Api/Api.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public class Api
+    {
+        //Ovu metodu obrisite kreirano je samo da mogu napraviti referencu na library
+        public void TestaKlasa ()
+        {
+           
+        }
+    }
+}
